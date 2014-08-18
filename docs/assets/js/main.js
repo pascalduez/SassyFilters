024af61864f542c0ae942b03482deb1d3c81d7f9
Pascal Duez <pascal@duez.me>
2014-08-18 17:16:10 +0200

    Update gh-pages

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,9 +1,34 @@
 /* global document, Fuse */
 'use strict';
 
+var addEvent = (function () {
+  var filter = function(el, type, fn) {
+    for ( var i = 0, len = el.length; i < len; i++ ) {
+      addEvent(el[i], type, fn);
+    }
+  };
+  if ( document.addEventListener ) {
+    return function (el, type, fn) {
+      if ( el && el.nodeName || el === window ) {
+        el.addEventListener(type, fn, false);
+      } else if (el && el.length) {
+        filter(el, type, fn);
+      }
+    };
+  }
+
+  return function (el, type, fn) {
+    if ( el && el.nodeName || el === window ) {
+      el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
+    } else if ( el && el.length ) {
+      filter(el, type, fn);
+    }
+  };
+})();
+
 var search = (function () {
   var getItems = function () {
-    return Array.prototype.slice.call(document.querySelectorAll('.js-sassdoc__item')).map(function (item) {
+    return Array.prototype.slice.call(document.querySelectorAll('.sassdoc__item')).map(function (item) {
       return {
         name: item.dataset.name,
         type: item.dataset.type,
@@ -37,7 +62,7 @@ var search = (function () {
     searchSuggestions.innerHTML = '';
     suggestions = items.slice(0, 10).map(function (item) {
       var li = document.createElement('li');
-      
+
       li.dataset.type = item.type;
       li.dataset.name = item.name;
       li.innerHTML = '<a href="#' + item.type + '-' + item.name + '"><code>' + item.type.slice(0, 3) + '</code> ' + item.name + '</a>';
@@ -101,7 +126,7 @@ var search = (function () {
     if (e.keyCode !== 40 && e.keyCode !== 38) {
       currentSelection = -1;
       performSearch(searchInput.value);
-    } 
+    }
 
     else {
       e.preventDefault();
@@ -112,3 +137,23 @@ var search = (function () {
     performSearch(searchInput.value);
   });
 })(search);
+
+(function (global) {
+  var classCollapsed = 'code--collapsed';
+  var classExpanded = 'code--expanded';
+  var parent;
+
+  global.addEvent(document.querySelectorAll('.item__code'), 'click', function () {
+    parent = this.parentNode;
+
+    if (parent.classList.contains(classExpanded)) {
+      parent.classList.remove(classExpanded);
+      parent.classList.add(classCollapsed);
+    }
+
+    else if (parent.classList.contains(classCollapsed)) {
+      parent.classList.remove(classCollapsed);
+      parent.classList.add(classExpanded);
+    }
+  });
+}(window));
